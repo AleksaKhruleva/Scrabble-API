2a61c1dc3d5c528b8ab6603d98ae8429fdb78acf
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2024-11-12 13:28:36 +0300

    fix: changed User model
    now conforming to Content protocol (for creating UsersController)

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -1,7 +1,7 @@
 import Fluent
 import Vapor
 
-final class User: Model, @unchecked Sendable {
+final class User: Model, Content, @unchecked Sendable {
     static let schema = Schema.users.rawValue
     
     @ID(key: .id)
