c24002ca86ef2ff416bf9b078463b0c93d35e87f
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2024-11-13 18:32:11 +0300

    deleted ExampleAuthUsageController

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,5 +12,4 @@ func routes(_ app: Application) throws {
 
     try app.register(collection: AuthController())
     try app.register(collection: UsersController())
-    try app.register(collection: ExampleAuthUsageController())
 }
